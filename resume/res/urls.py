581a05fa4e44439b222469e97d195dff62c07b84
SSSM22 <saishivamani.s@gmail.com>
2024-07-01 12:01:11 +0530

    initial setup

---
+++ resume/res/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+from . import views
+from django.contrib.auth import views as auth_views
+from django.views.static import serve
+from django.conf import settings
+from django.urls import re_path
+urlpatterns=[
+    path('',views.index,name='index'),
+]
